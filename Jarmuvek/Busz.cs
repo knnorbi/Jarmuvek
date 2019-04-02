85512333ff5fd4bd5785f5d74646b86f29aefcc2
Norbert Kluka <kluka.norbert@gmail.com>
2019-04-02 11:54:03 +0200

    IMegrendelheto added

--- Jarmuvek/Busz.cs
+++ Jarmuvek/Busz.cs
@@ -6,15 +6,53 @@ using System.Threading.Tasks;
 
 namespace Jarmuvek
 {
-    class Busz : Jarmu
+    class Busz : Jarmu, IMegrendelheto
     {
         int ferohelyekSzama;
+        int alapAr;
+        int kmDij;
+        bool szabad;
 
-        public Busz(string rendszam, string marka, string tipus, Szin szin, int ferohelyekSzama) : base(rendszam, marka, tipus, szin)
+        public Busz(string rendszam, string marka, string tipus, Szin szin, int ferohelyekSzama, int alapAr, int kmDij) : base(rendszam, marka, tipus, szin)
         {
             this.ferohelyekSzama = ferohelyekSzama;
+            this.alapAr = alapAr;
+            this.kmDij = kmDij;
+            szabad = true;
         }
 
         public int FerohelyekSzama { get { return ferohelyekSzama; } }
+
+        public int AlapAr { get { return alapAr; } }
+
+        public int KmDij { get { return kmDij; } }
+
+        public bool Szabad { get { return szabad; } }
+
+        public int Megrendel(int km)
+        {
+            if (szabad)
+            {
+                szabad = false;
+                return alapAr + km * kmDij;
+            }
+            else
+            {
+                // TODO
+                return 0;
+            }
+        }
+
+        public void Visszaerekezik()
+        {
+            if (!szabad)
+            {
+                szabad = true;
+            }
+            else
+            {
+                // TODO
+            }
+        }
     }
 }
---
+++ Jarmuvek/IMegrendelheto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jarmuvek
+{
+    interface IMegrendelheto
+    {
+        int AlapAr { get; }
+        int KmDij { get; }
+        bool Szabad { get; }
+
+        int Megrendel(int km);
+        void Visszaerekezik();
+    }
+}
